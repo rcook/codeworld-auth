7bd6bbc07a8a02537a5c77c42d00d2a6bf83b6b7
Richard Cook <rcook@rcook.org>
2018-05-11 07:14:09 -0700

    Implement GoogleAuth

--- lib/CodeWorld/Auth/GoogleAuth.hs
+++ lib/CodeWorld/Auth/GoogleAuth.hs
@@ -11,6 +11,8 @@
   limitations under the License.
 -}
 
+{-# LANGUAGE OverloadedStrings #-}
+
 module CodeWorld.Auth.GoogleAuth
     ( AuthConfig
     , authRoutes
@@ -19,22 +21,81 @@ module CodeWorld.Auth.GoogleAuth
     , optionallyAuthenticated
     ) where
 
-import           CodeWorld.Account (UserId)
+import           CodeWorld.Account (UserId(..))
+import           CodeWorld.Auth.Http
 import           CodeWorld.Auth.Types
-import           Snap.Core (Snap)
+import           Control.Monad (mzero, when)
+import           Control.Monad.IO.Class (liftIO)
+import           Data.Aeson
+                    ( (.:)
+                    , FromJSON
+                    , Value(..)
+                    , decode
+                    , parseJSON
+                    )
+import           Data.ByteString (ByteString)
+import qualified Data.ByteString.Char8 as Char8 (unpack)
+import           Data.Text (Text)
+import qualified Data.Text as Text (strip)
+import qualified Data.Text.IO as Text (readFile)
+import           Network.HTTP.Conduit (simpleHttp)
+import           Snap.Core (Snap, finishWith, getParam, pass)
+import           System.Directory (doesFileExist)
+import           System.FilePath ((</>))
 
-data AuthConfig
+newtype ClientId = ClientId Text deriving Eq
+
+data AuthConfig = AuthConfig ClientId
+
+data User = User UserId Text
+
+instance FromJSON User where
+    parseJSON (Object v) = User <$> (UserId <$> v .: "user_id") <*> v .: "audience"
+    parseJSON _ = mzero
 
 configureAuth :: FilePath -> IO (Maybe AuthConfig)
-configureAuth _ = do
-    putStrLn "Google authentication not implemented!"
-    pure Nothing
+configureAuth appDir = do
+    let clientIdPath = appDir </> "web/clientId.txt"
+    clientIdPathExists <- doesFileExist clientIdPath
+    case clientIdPathExists of
+        False -> do
+            putStrLn $ "Client ID file not found at " ++ clientIdPath ++ ": skipping configuration of Google authentication"
+            pure Nothing
+        True -> do
+            clientIdContent <- Text.readFile "web/clientId.txt"
+            let clientId = ClientId $ Text.strip clientIdContent
+            pure $ Just (AuthConfig clientId)
 
 authRoutes :: AuthConfig -> [Route]
-authRoutes _ = undefined
+authRoutes _ = []
 
 authenticated :: (UserId -> Snap ()) -> AuthConfig -> Snap ()
-authenticated = undefined
+authenticated handler authConfig = do
+    mbIdToken <- getParam "id_token"
+    case mbIdToken of
+        Nothing -> finishWith unauthorized401
+        Just idToken -> do
+            userId  <- authenticatedHelper authConfig idToken
+            handler userId
 
 optionallyAuthenticated :: (Maybe UserId -> Snap ()) -> AuthConfig -> Snap ()
-optionallyAuthenticated = undefined
+optionallyAuthenticated handler authConfig = do
+    mbIdToken <- getParam "id_token"
+    case mbIdToken of
+        Nothing -> handler Nothing
+        Just idToken -> do
+            userId  <- authenticatedHelper authConfig idToken
+            handler (Just userId)
+
+-- Retrieves the user for the current request. The request should have an
+-- id_token parameter with an ID token retrieved from the Google
+-- authentication API. The user is returned if the ID token is valid.
+authenticatedHelper :: AuthConfig -> ByteString -> Snap UserId
+authenticatedHelper (AuthConfig clientId) idToken = do
+    let url = "https://www.googleapis.com/oauth2/v1/tokeninfo?id_token=" ++ Char8.unpack idToken
+    result <- fmap decode $ liftIO $ simpleHttp url
+    case result of
+        Nothing -> pass
+        Just (User userId audience) -> do
+            when (clientId /= ClientId audience) pass
+            return userId
